e1c07f8b0280716d9795b84c451c0b26dc061dc9
bharat948 <bharatpatidar.002@gmail.com>
2024-12-20 12:36:57 +0530

    day-1 and day-2

---
+++ day2/zero_values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var y int
+	var z bool
+	var aa string
+	var bb *int
+	fmt.Println("Y=", y)
+	fmt.Println("z=", z)
+	fmt.Println("aa=", aa)
+	fmt.Println("bb=", bb)
+}
